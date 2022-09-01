1e38617a0d1cbab7f441a662aad163442029aadd
Hayeong-Hong <rack_roll@naver.com>
2022-09-01 16:50:03 +0900

    HY 220901 1700 Stepping2

--- BearBom/src/main/frontend/src/PageComponents/CourseRegistration/StepThree_1.js
+++ BearBom/src/main/frontend/src/PageComponents/CourseRegistration/StepThree_1.js
@@ -1,79 +1,125 @@
 import {
-    Button,
-    FormControl,
-    InputLabel,
-    MenuItem,
-    Select,
-  } from "@mui/material";
-  import React, { useState } from "react";
-  import "../../css/courseRegistration.css";
-import KakaoAPI from "./RegistrationComponents/KakaoAPI";
-  
-  const StepThree_1 = () => {
-    const [location, setLocation] = useState("");
-  
-    const handleChangeLoca = (event) => {
-      setLocation(event.target.value);
-    };
-  
-    return (
-      <>
-        <div className="content content1">
-          <div className="contentName">Step.3 금액 및 일정</div>
-          <div className="contentWrap">
-            <div className="contentDetail">
-              <div className="nameWrap">
-                <h5 className="detailName">
-                  클래스 일정 및 인원
-                  <div className="nameUnderbar"></div>
-                </h5>
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
+import React, { useState } from "react";
+import "../../css/courseRegistration.css";
+
+const range = (start, stop, step) => {
+  let a = [start],
+    b = start;
+  while (b < stop) {
+    a.push((b += step || 1));
+  }
+  return a;
+};
+
+const StepThree_1 = () => {
+  const [minPeople, setMinPeople] = useState();
+  const [maxPeople, setMaxPeople] = useState();
+
+  const handleMinPeople = (event) => {
+    setMinPeople(event.target.value);
+  };
+
+  const handleMaxPeople = (event) => {
+    setMaxPeople(event.target.value);
+  };
+
+  return (
+    <>
+      <div className="content content1">
+        <div className="contentName">Step.3 금액 및 일정</div>
+        <div className="contentWrap">
+          <div className="contentDetail">
+            <div className="nameWrap">
+              <h5 className="detailName">
+                클래스 일정 및 인원
+                <div className="nameUnderbar"></div>
+              </h5>
+            </div>
+            <div className="detailEx">
+              <p className="datilNameInfo">
+                클래스의 일정과 수강인원을 설정해 주세요
+              </p>
+            </div>
+            <div className="detail">
+              <div className="numCheck">
+                <div className="datailLabel">
+                  <p>클래스 일정</p>
+                </div>
+                <div>여기에 캘린더 추가</div>
+                <p className="inputWar">*일정 지정 시 유의사항 적는곳.</p>
               </div>
-              <div className="detailEx">
-                <p className="datilNameInfo">클래스의 일정과 수강인원을 설정해 주세요</p>
-              </div>
-              <div className="detail">
-                <div className="selectBox">
+              <div className="numCheck">
+                <div className="datailLabel">
+                  <p>클래스 인원</p>
+                </div>
+                <div className="minMaxBox">
                   <FormControl
-                    sx={{ minWidth: 280 }}
+                    sx={{ minWidth: 150 }}
                     style={{ marginLeft: 20 }}
                     size="small"
                   >
                     <InputLabel id="demo-simple-select-label">
-                      지역선택
+                      최소 인원
                     </InputLabel>
                     <Select
                       labelId="demo-simple-select-label"
                       id="demo-simple-select"
-                      value={location}
+                      value={minPeople}
                       label="Cate"
-                      onChange={handleChangeLoca}
+                      onChange={handleMinPeople}
                     >
-                      <MenuItem value={201}>전체</MenuItem>
-                      <MenuItem value={202}>서울</MenuItem>
-                      <MenuItem value={203}>경기</MenuItem>
-                      <MenuItem value={204}>인천</MenuItem>
-                      <MenuItem value={205}>강원</MenuItem>
-                      <MenuItem value={206}>대구</MenuItem>
-                      <MenuItem value={207}>부산</MenuItem>
-                      <MenuItem value={208}>경상북도</MenuItem>
-                      <MenuItem value={209}>경상남도</MenuItem>
-                      <MenuItem value={210}>울산</MenuItem>
-                      <MenuItem value={211}>광주</MenuItem>
-                      <MenuItem value={212}>전라북도</MenuItem>
-                      <MenuItem value={213}>전라남도</MenuItem>
-                      <MenuItem value={214}>세종</MenuItem>
-                      <MenuItem value={215}>제주</MenuItem>
-                      <MenuItem value={216}>충청북도</MenuItem>
-                      <MenuItem value={217}>충청남도</MenuItem>
+                      <MenuItem disabled value={0}>
+                        최소 인원
+                      </MenuItem>
+                      {range(1, 20, 1).map((item) => (
+                        <MenuItem value={item}>{item}명</MenuItem>
+                      ))}
+                      {range(30, 100, 10).map((item) => (
+                        <MenuItem value={item}>{item}명</MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <div>/</div>
+                  <FormControl
+                    sx={{ minWidth: 150 }}
+                    style={{ marginLeft: 0 }}
+                    size="small"
+                  >
+                    <InputLabel id="demo-simple-select-label">
+                      최대 인원
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      value={maxPeople}
+                      label="Cate"
+                      onChange={handleMaxPeople}
+                    >
+                      <MenuItem disabled value={0}>
+                        최대 인원
+                      </MenuItem>
+                      {range(1, 20, 1).map((item) => (
+                        <MenuItem value={item}>{item}명</MenuItem>
+                      ))}
+                      {range(30, 100, 10).map((item) => (
+                        <MenuItem value={item}>{item}명</MenuItem>
+                      ))}
                     </Select>
                   </FormControl>
                 </div>
+                <p className="inputWar">*인원 지정 시 유의사항 적는곳.</p>
               </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  };
-  export default StepThree_1;
-  
+      </div>
+    </>
+  );
+};
+export default StepThree_1;
