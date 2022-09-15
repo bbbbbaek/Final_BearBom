f3083f4552d53342870efd59340e3d80a4e248eb
moonjaeh <mooooo1997@naver.com>
2022-09-15 14:03:51 +0900

    관리자백앤드추가

---
+++ BearBom/src/main/java/com/spring/bearbom/service/admin/AdminService.java
@@ -0,0 +1,11 @@
+package com.spring.bearbom.service.admin;
+
+import java.util.List;
+
+import com.spring.bearbom.entity.Inquiry;
+
+public interface AdminService {
+
+	List<Inquiry> inquiryInfoReference(Inquiry inquiry);
+
+}
