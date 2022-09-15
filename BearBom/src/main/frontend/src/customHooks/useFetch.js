b302a6cdb32ab92106afec2f34fa314339235a7d
김광민 <kenny@gimgwangmin-ui-MacBookAir.local>
2022-09-15 14:27:13 +0900

    220915 ing

--- BearBom/src/main/frontend/src/customHooks/useFetch.js
+++ BearBom/src/main/frontend/src/customHooks/useFetch.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
 
@@ -6,10 +7,32 @@ const useFetch = (url) => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     setLoading(true);
-  //     const res = axios.get(url);
-  //   };
-  // });
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetch };
 };
+
+export default useFetch;
