03b3459a7603daa77c38dc0dfe070a16e15f9f78
Debonchocola <pub10@naver.com>
2022-09-21 19:36:35 +0900

    우빈_0921

---
+++ BearBom/src/main/frontend/src/PageComponents/Detail/PayWindow.js
@@ -0,0 +1,9 @@
+function PayWindow() {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+}
+
+export default PayWindow;
--- BearBom/src/main/java/com/spring/bearbom/mapper/MypageMapper.java
+++ BearBom/src/main/java/com/spring/bearbom/mapper/MypageMapper.java
@@ -13,14 +13,13 @@ import com.spring.bearbom.dto.InquiryDTO;
 @Mapper
 public interface MypageMapper {
 	
-	@Select("SELECT A.* FROM T_COURSE A,(SELECT * FROM T_LIKE) B WHERE B.USER_ID = #{userId} AND A.COURSE_IDX = B.COURSE_IDX")
+//	@Select("SELECT A.* FROM T_COURSE A,(SELECT * FROM T_LIKE) B WHERE B.USER_ID = #{userId} AND A.COURSE_IDX = B.COURSE_IDX")
 	public List<CourseDTO> getWishList( CourseDTO courseDTO);
 		
-	
-	
-//	@Select("SELECT COUNT(*) AS CNT FROM T_COURSE A, (SELECT * FROM T_LIKE) B WHERE A.USER_ID = #{userId} AND A.COURSE_IDX = B.COURSE_IDX")
-	@Select("SELECT COUNT(A.course_idx) AS CNT FROM T_COURSE A, (SELECT * FROM T_LIKE) B WHERE A.USER_ID = #{userId} AND A.COURSE_IDX = B.COURSE_IDX")
+//	@Select("SELECT COUNT(*) AS CNT FROM T_COURSE A,T_LIKE B WHERE B.USER_ID = #{userId} AND A.COURSE_IDX = B.COURSE_IDX")
 	public List<CourseDTO> getWishCnt(CourseDTO courseDTO);
+//	@Select("SELECT COUNT(A.course_idx) AS CNT FROM T_COURSE A, (SELECT * FROM T_LIKE) B WHERE A.USER_ID = #{userId} AND A.COURSE_IDX = B.COURSE_IDX")
+	
 
     List<Map<String, Object>> getInquiryReference(String userId);
 }
