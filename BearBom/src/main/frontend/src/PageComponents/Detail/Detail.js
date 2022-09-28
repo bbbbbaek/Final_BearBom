dc493cfa03c9264b30940ae1350670d61f2c96af
Debonchocola <pub10@naver.com>
2022-09-28 13:50:50 +0900

    0928

--- BearBom/src/main/frontend/src/PageComponents/Detail/Detail.js
+++ BearBom/src/main/frontend/src/PageComponents/Detail/Detail.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useLocation, useParams } from "react-router-dom";
 import "../../css/detail.css";
 import { API_BASE_URL } from "../../app-config";
 import axios from "axios";
